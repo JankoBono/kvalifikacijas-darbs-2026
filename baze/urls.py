2fa86d1984e35d10200dd98516c3b5e44d8741af
Jānis Kokle <kokle.janis@gmail.com>
2025-10-12 15:40:35 +0300

    pabeigti darijumu crud un pievienota login lapa

--- baze/urls.py
+++ baze/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
     path("", views.index, name="index"),
     path('add-darijums/', views.addDarijums, name="add-darijums"),
+    path('edit-darijums/<str:pk>/', views.editDarijums, name="edit-darijums"),
+    path('delete-darijums/<str:pk>/', views.deleteDarijums, name="delete-darijums"),
 ]
--- baze/views.py
+++ baze/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .models import Darijums
 from .forms import DarijumsForm
+
+
+
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Lietotājs neeksistē')
+
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Neveiksmīga pieslēgšanās')
+            
+    context = {}
+    return render(request, 'baze/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('index')
 
 def index(request):
     darijumi = Darijums.objects.all()
@@ -19,3 +49,23 @@ def addDarijums(request):
 
     context = {'form': form}
     return render(request, 'baze/darijums_add.html', context)
+
+def editDarijums(request, pk):
+    darijums = Darijums.objects.get(id=pk)
+    form = DarijumsForm(instance=darijums)
+
+    if request.method == 'POST':
+        form = DarijumsForm(request.POST, instance=darijums)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    context = {'form' : form}
+    return render(request, 'baze/darijums_add.html', context)
+
+def deleteDarijums(request, pk):
+    darijums = Darijums.objects.get(id=pk)
+    if request.method == "POST":
+        darijums.delete()
+        return redirect('index')
+    return render(request, 'baze/delete.html', {'obj':darijums})
